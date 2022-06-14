a25c43c8c46f17a7b67a448011e6525d8d7aa692
Christoph Howischer <christoph.howischer@erfideo.com>
2022-06-14 06:46:15 +0200

    added V1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace StartAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: StartAll <StartDir>");
+                Console.ReadLine();
+                return;
+            }
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine($"Directory {args[0]} not found");
+                Console.ReadLine();
+                return;
+            }
+            string[] files = Directory.GetFiles(args[0], "*.exe", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = file;
+                psi.CreateNoWindow = true;
+                Process.Start(psi);
+            }
+        }
+    }
+}
